5fd7fe212ee1e8708216049e654e315adba07888
Marco Lussetti <marcolussetti@users.noreply.github.com>
2020-10-21 19:00:13 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,211 @@
+<script>
+	import { onMount } from 'svelte';
+	
+	const games = [
+		// Streamed via Discord
+		// Jackbox 4
+		{
+			"name": "Fibbage 3",
+			"min_players": 2,
+			"max_players": 8,
+			"requires": "Jackbox 4",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Fibbage Enough about You",
+			"min_players": 2,
+			"max_players": 8,
+			"requires": "Jackbox 4",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Survive the Internet",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Jackbox 4",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Monster Seeking Monster",
+			"min_players": 3,
+			"max_players": 7,
+			"requires": "Jackbox 4",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Bracketeering",
+			"min_players": 3,
+			"max_players": 16,
+			"requires": "Jackbox 4",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Civic Doodle",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Jackbox 4",
+			"requires_streaming": true,
+		},
+		// Jackbox 5
+		{
+			"name": "You dont know jack",
+			"min_players": 1,
+			"max_players": 8,
+			"requires": "Jackbox 5",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Split the room",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Jackbox 5",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Mad verse city",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Jackbox 5",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Patently stupid",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Jackbox 5",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Zeeple dome",
+			"min_players": 1,
+			"max_players": 6,
+			"requires": "Jackbox 5",
+			"requires_streaming": true,
+		},
+		// Jackbox 6
+		{
+			"name": "Trivia murder party 2",
+			"min_players": 1,
+			"max_players": 8,
+			"requires": "Jackbox 6",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Role models",
+			"min_players": 3,
+			"max_players": 6,
+			"requires": "Jackbox 6",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Joke boat",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Jackbox 6",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Dictionarium",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Jackbox 6",
+			"requires_streaming": true,
+		},
+		{
+			"name": "Push the button",
+			"min_players": 4,
+			"max_players": 10,
+			"requires": "Jackbox 6",
+			"requires_streaming": true,
+		},
+		// Steam
+		{
+			"name": "Drawful",
+			"min_players": 3,
+			"max_players": 8,
+			"requires": "Drawful",
+			"requires_streaming": true,
+		},
+		
+		// Thick client
+		// Steam (Win, Mac, Lin)
+		{
+			"name": "Clue",
+			"min_players": 3,
+			"max_players": 6,
+			"requires": "Tabletop Simulator",
+			"requires_streaming": false,
+		},
+		{
+			"name": "Deceit",
+			"min_players": 6,
+			"max_players": 6,
+			"requires": "Deceit",
+			"requires_streaming": true,
+		},
+		// Steam (Win), Mobile (Android, iOS)
+		{
+			"name": "Among Us",
+			"min_players": 4,
+			"max_players": 10,
+			"requires": "Among Us",
+			"requires_streaming": false,
+		},
+		
+		// Web client
+		// Netgames
+		{
+			"name": "One Night Werewolf",
+			"min_players": 3,
+			"max_players": 21,
+			"requires": "N/A",
+			"requires_streaming": false,
+		},
+		{
+			"name": "Codewords",
+			"min_players": 4,
+			"max_players": 100,
+			"requires": "N/A",
+			"requires_streaming": false,
+		},
+		// Secret Hitler
+		{
+			"name": "Secret Hitler",
+			"min_players": 5,
+			"max_players": 10,
+			"requires": "N/A",
+			"requires_streaming": false,
+		},
+		
+	]
+	
+	let game = "...";
+	let chosenGame = false;
+	
+	const shuffleGame = () => {
+		game = "[...]"
+		setTimeout(() => {
+			game = "[shuffling dices...]";
+			setTimeout(() => {
+				game = "[rolling cards...]";
+				setTimeout(() => {
+					chosenGame = true;
+					gameObj = games[Math.floor(Math.random() * games.length)]
+					game = gameObj["name"]
+				}, 1500)
+			}, 1500)
+		}, 1500)
+	}
+	
+	onMount(() => {
+		shuffleGame()
+	});
+</script>
+
+<h2>We are playing <i>{game}</i></h2>
+
+{#if chosenGame}
+<button on:click={() => shuffleGame()}>
+	Thx I hate it
+</button>
+{/if}
