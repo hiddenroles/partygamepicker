45d2dc50e2f3768fd2e0c08e0715b15253bd9657
Marco Lussetti <marcolussetti@users.noreply.github.com>
2020-10-23 19:29:40 -0700

    clean up files

--- src/App.svelte
+++ src/App.svelte
@@ -28,74 +28,75 @@
 	// });
 </script>
 
-<a class="github-fork-ribbon" href="https://github.com/marcolussetti/partygamepicker" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
+<a class="github-fork-ribbon" data-ribbon="Fork me on GitHub" href="https://github.com/marcolussetti/partygamepicker"
+	 title="Fork me on GitHub">Fork me on GitHub</a>
 <section class="hero is-light is-fullheight">
-    <div class="hero-body">
-        <div class="container">
-            <h1 class="title">
-                We are playing <i>{gameMessage}</i>
-            </h1>
-            {#if game}
-                <div class="box">
+	<div class="hero-body">
+		<div class="container">
+			<h1 class="title">
+				We are playing <i>{gameMessage}</i>
+			</h1>
+			{#if game}
+				<div class="box">
 					<div class="container">
-                        <h1 class="subtitle has-text-centered is-italic"><p><a href="{game.url}">{game.name}</a></p></h1>
+						<h1 class="subtitle has-text-centered is-italic"><p><a href="{game.url}">{game.name}</a></p></h1>
 					</div>
-                    <div class="columns">
-                        <div class="column">
-                            <nav class="panel">
-                                <div class="panel-block">
-                                    <div class="container is-fluid">
-                                        <div class="columns">
-                                            <div class="column is-one-third has-text-weight-bold">Min Players:</div>
-                                            <div class="column is-two-thirds">{game.min_players}</div>
-                                        </div>
-                                    </div>
-                                </div>
+					<div class="columns">
+						<div class="column">
+							<nav class="panel">
+								<div class="panel-block">
+									<div class="container is-fluid">
+										<div class="columns">
+											<div class="column is-one-third has-text-weight-bold">Min Players:</div>
+											<div class="column is-two-thirds">{game.min_players}</div>
+										</div>
+									</div>
+								</div>
 
-                                <div class="panel-block">
-                                    <div class="container is-fluid">
-                                        <div class="columns">
-                                            <div class="column is-one-third has-text-weight-bold">Max Players:</div>
-                                            <div class="column is-two-thirds">{game.max_players}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
+								<div class="panel-block">
+									<div class="container is-fluid">
+										<div class="columns">
+											<div class="column is-one-third has-text-weight-bold">Max Players:</div>
+											<div class="column is-two-thirds">{game.max_players}</div>
+										</div>
+									</div>
+								</div>
+							</nav>
+						</div>
 
-                        <div class="column">
-                            <nav class="panel">
-                                <div class="panel-block">
-                                    <div class="container is-fluid">
-                                        <div class="columns">
-                                            <div class="column is-one-third has-text-weight-bold">Requires:</div>
-                                            <div class="column is-two-thirds">{game.requires}</div>
-                                        </div>
-                                    </div>
-                                </div>
+						<div class="column">
+							<nav class="panel">
+								<div class="panel-block">
+									<div class="container is-fluid">
+										<div class="columns">
+											<div class="column is-one-third has-text-weight-bold">Requires:</div>
+											<div class="column is-two-thirds">{game.requires}</div>
+										</div>
+									</div>
+								</div>
 
-                                <div class="panel-block">
-                                    <div class="container is-fluid">
-                                        <div class="columns">
-                                            <div class="column is-one-third has-text-weight-bold">Requires streaming:
-                                            </div>
-                                            <div class="column is-two-thirds">{game.requires_streaming ? "yes" : "no"}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            {/if}
+								<div class="panel-block">
+									<div class="container is-fluid">
+										<div class="columns">
+											<div class="column is-one-third has-text-weight-bold">Requires streaming:
+											</div>
+											<div class="column is-two-thirds">{game.requires_streaming ? "yes" : "no"}</div>
+										</div>
+									</div>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			{/if}
 
-            <button on:click={() => shuffleGame()}>
-                {#if chosenGame}
-                    Thx I hate it
-                {:else}
-                    Give me a game
-                {/if}
-            </button>
-        </div>
-    </div>
+			<button on:click={() => shuffleGame()}>
+				{#if chosenGame}
+					Thx I hate it
+				{:else}
+					Give me a game
+				{/if}
+			</button>
+		</div>
+	</div>
 </section>
